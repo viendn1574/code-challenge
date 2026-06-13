d8ad2b39492b315ebce9da8672a77d6748f56997
VienNguyen <viennd1574@gmail.com>
2026-06-13 11:42:51 +0700

    Add problem4

---
+++ problem4/README.md
@@ -0,0 +1,17 @@
+# Problem 4: Three ways to sum to n
+Provide 3 unique implementations of the following function in TypeScript.
+
+- Comment on the complexity or efficiency of each function.
+
+**Input**: `n` - any integer
+
+*Assuming this input will always produce a result lesser than `Number.MAX_SAFE_INTEGER`*.
+
+**Output**: `return` - summation to `n`, i.e. `sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15`.
+
+## Usage
+
+```bash
+npm install
+npm run dev
+```
---
+++ problem4/main.ts
@@ -0,0 +1,46 @@
+function sum_to_n_a(n: number): number {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+function sum_to_n_b(n: number): number {
+    if (n === 1 || n === 0) {
+        return n;
+    }
+    return n + sum_to_n_b(n - 1);
+}
+
+function sum_to_n_c(n: number): number {
+    return (n * (n + 1)) / 2;
+}
+
+
+
+let n;
+// Test cases 1 input: 0
+n = 0;
+console.log(`Sum from 1 to ${n} using method A: ${sum_to_n_a(n)}`);
+console.log(`Sum from 1 to ${n} using method B: ${sum_to_n_b(n)}`);
+console.log(`Sum from 1 to ${n} using method C: ${sum_to_n_c(n)}`); 
+
+// Test cases 2 input: 1
+n = 1;
+console.log(`Sum from 1 to ${n} using method A: ${sum_to_n_a(n)}`);
+console.log(`Sum from 1 to ${n} using method B: ${sum_to_n_b(n)}`);
+console.log(`Sum from 1 to ${n} using method C: ${sum_to_n_c(n)}`);
+
+// Test cases 3 input: 5
+n = 5;
+console.log(`Sum from 1 to ${n} using method A: ${sum_to_n_a(n)}`);
+console.log(`Sum from 1 to ${n} using method B: ${sum_to_n_b(n)}`);
+console.log(`Sum from 1 to ${n} using method C: ${sum_to_n_c(n)}`);
+
+// Test cases 4 input: 1000000
+// With large n, method B will cause a stack overflow due to too much recursion, so we will not run it for n = 1000000
+n = 1000000;
+console.log(`Sum from 1 to ${n} using method A: ${sum_to_n_a(n)}`);
+console.log(`Sum from 1 to ${n} using method B: ${sum_to_n_c(n)}`);
+//console.log(`Sum from 1 to ${n} using method C: ${sum_to_n_b(n)}`);
